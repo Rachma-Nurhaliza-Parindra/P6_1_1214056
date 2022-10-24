37cf5d2bba39c18397926c0f04992a89bfb29b28
Rachma <rachmanurhalizastudent@gmail.com>
2022-10-24 14:07:42 +0700

    Add project files.

---
+++ P6_1_1214056/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace P6_1_1214056
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label.Location = new System.Drawing.Point(89, 36);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(500, 25);
+            this.label.TabIndex = 0;
+            this.label.Text = "Which do you is better, Visual Basic or C# ?";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // vbButton
+            // 
+            this.vbButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.vbButton.Location = new System.Drawing.Point(150, 94);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(115, 23);
+            this.vbButton.TabIndex = 1;
+            this.vbButton.Text = "Visual Basic";
+            this.vbButton.UseVisualStyleBackColor = true;
+            this.vbButton.Click += new System.EventHandler(this.vbButton_Click);
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(411, 94);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(100, 23);
+            this.csButton.TabIndex = 2;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = true;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(42, 305);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.closeButton.Size = new System.Drawing.Size(100, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(684, 336);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Controls.Add(this.label);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button vbButton;
+        private System.Windows.Forms.Button csButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
